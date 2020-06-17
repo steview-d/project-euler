17c70d1483b3d861a743868b6f87d1657b8561fa
Dave Laffan <dave.laffan@outlook.com>
2020-06-17 10:03:20 +0100

    Update py solution for challenge 1

--- 1/1.py
+++ 1/1.py
@@ -1,9 +1,5 @@
-def counter(num):
-    count = 0
-    for i in range(1, num):
-        if i % 3 == 0 or i % 5 == 0:
-            count += i
-    return count
+def counter(n):
+    return(sum([i for i in range(n) if i % 3 == 0 or i % 5 == 0]))
 
 
 print("Count:", counter(1000))

bc942172c7349aa5f79c7d37d6a0d099589ac7d8
Dave Laffan <dave.laffan@outlook.com>
2020-06-17 12:23:51 +0100

    Add comments

--- 2/2.py
+++ 2/2.py
@@ -1,9 +1,11 @@
 def fibonacci_evens(limit):
     fs = [1, 2]
 
+    # create array of all fibonacci numbers below limit param
     while fs[-1] + fs[-2] < limit:
         fs.append(fs[-1] + fs[-2])
 
+    # sum all even numbers in array
     return(sum(n for n in fs if n % 2 == 0))
 
 
